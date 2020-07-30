917b180bc78c600a8416acabdf0f94f5491f1395
tctg0arnav <59003715+tctg0arnav@users.noreply.github.com>
2020-07-30 09:13:05 +0530

    anti cheat activate

--- xino/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("btn").onfocusout  = disableScreen(){
-    // creates <div class="overlay"></div> and 
-    // adds it to the DOM
-    var div= document.createElement("div");
-    div.className += "overlay";
-    document.body.appendChild(div);
-};
-
